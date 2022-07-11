eb36becd086a86d33b6e55dcc587c1e25b307d53
MustafaNsrv <86935457+MustafaNasirov@users.noreply.github.com>
2022-07-11 20:01:17 +0400

    added 2nd translator attempt

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const englishInp = document.querySelector(".morse-translator__input-field");
 const morseOut = document.querySelector(".morse-translator__output");
 const submitBtn = document.querySelector(".morse-translator__submit");
 
+const morseInp = document.querySelector("#transMorse");
+const engOut = document.querySelector("#showEng");
+
+
 const displayOutput = () => {
-    //let inpStr = englishInp.value;
     morseOut.innerHTML = translateMorse(englishInp.value);
 }; 
 
@@ -13,3 +16,4 @@ submitBtn.addEventListener("click", displayOutput);
 console.log(englishInp.value)
 
 
+// had issues with styling and DOM, ran out of time for second translator :|
